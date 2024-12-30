846c3c405e068b9c5883bd45f8ab2b1d8ab8fdcc
Marvin SH <mardevsh@gmail.com>
2024-12-30 15:47:11 -0600

    Agregado de tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Archivos de React
+    "./public/index.html", // Archivo HTML principal (si existe)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
